580684066cadacef43db3dd767707bdb9aaaa864
Jawad Hussein <jawaddhussein@gmail.com>
2019-01-29 12:49:00 -0800

    Git push test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+// const containerBtn = document.querySelector('.container');
+// containerBtn.addEventListener('click', e => {
+//   // target is what you are actively clicking
+//   console.log(`This is my target: ${e.target}`);
+
+
